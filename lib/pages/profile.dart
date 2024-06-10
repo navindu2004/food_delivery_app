26f93d5517289007ba2bbdcbccd4f7aee6054f04
Navindu Nimnal Premaratne <130588741+navindu2004@users.noreply.github.com>
2024-06-11 04:30:40 +0530

    chore: Import necessary packages and update file paths

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp_02/service/auth.dart';
 import 'package:flutterapp_02/service/shared_pref.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:random_string/random_string.dart';
